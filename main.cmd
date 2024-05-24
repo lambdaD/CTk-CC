d1dd7d5fc01c24523b84d833cc32b6e7b77ca661
lambdaD <joyitdust@gmail.com>
2024-05-24 07:16:11 +0500

    Скомпилирован

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=C:\Users\MAXSHR~1\AppData\Local\Programs\Python\PYTHON~2;%PATH%
+set PYTHONHOME=C:\Users\maxshroomer\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\maxshroomer\Desktop\CTk CC;C:\Users\maxshroomer\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\maxshroomer\AppData\Local\Programs\Python\Python312\Lib;C:\Users\maxshroomer\AppData\Local\Programs\Python\Python312;C:\Users\maxshroomer\AppData\Local\Programs\Python\Python312\Lib\site-packages
+"%~dp0main.exe" %*
