2d89e5e7b05f245a3c011bb13e944dbf8e2338ab
lambdaD <joyitdust@gmail.com>
2024-05-26 02:36:02 +0500

    1

--- tabWindow.py
+++ tabWindow.py
@@ -2,39 +2,33 @@ from tkinter import *
 from tkinter import ttk
 from iflang import *
 
+
+
 settings = include_settings()
 lang = changeLanguage(settings['language']['user_language'])
 
-# Функция для создания плана выплат (окно)
+
 def create_new_tab_windowSchedulePayments(payments):
     windowSchedulePayments = Toplevel()
     windowSchedulePayments.title(lang['ScheduleTable']['Title'])
-
-    # Определяем столбцы
     columns = [lang['ScheduleTable']['Num'], 
                lang['ScheduleTable']['Date'], 
                lang['ScheduleTable']['Payment'], 
                lang['ScheduleTable']['Principal'], 
                lang['ScheduleTable']['Interest'], 
                lang['ScheduleTable']['Balance']]
-
     tableSchedulePayments = ttk.Treeview(windowSchedulePayments, columns=columns, show='headings')
-
-    # Настраиваем заголовки столбцов
     tableSchedulePayments.heading(lang['ScheduleTable']['Num'], text=lang['ScheduleTable']['Num'], anchor=CENTER)
     tableSchedulePayments.heading(lang['ScheduleTable']['Date'], text=lang['ScheduleTable']['Date'], anchor=CENTER)
     tableSchedulePayments.heading(lang['ScheduleTable']['Payment'], text=lang['ScheduleTable']['Payment'], anchor=CENTER)
     tableSchedulePayments.heading(lang['ScheduleTable']['Principal'], text=lang['ScheduleTable']['Principal'], anchor=CENTER)
     tableSchedulePayments.heading(lang['ScheduleTable']['Interest'], text=lang['ScheduleTable']['Interest'], anchor=CENTER)
     tableSchedulePayments.heading(lang['ScheduleTable']['Balance'], text=lang['ScheduleTable']['Balance'], anchor=CENTER)
-
-    # Настраиваем столбцы
     for col in columns:
         tableSchedulePayments.column(col, anchor=CENTER)
-
-    # Добавляем таблицу в окно
     tableSchedulePayments.pack(fill='both', expand=True)
-
-    # Добавляем данные в таблицу
     for payment in payments:
         tableSchedulePayments.insert('', 'end', values=payment)
+
+
+
